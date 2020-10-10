b5aed31eb5e91c21c8cf21ab8f3525ec1c4c31d1
Desprez Thibault <thibault.desprez1@gmail.com>
2020-10-10 22:17:56 +0200

    patch lyon

--- getStations.py
+++ getStations.py
@@ -1,6 +1,7 @@
 import requests
 import json
-from pprint import pprint
+from pymongo import MongoClient
+
 
 def get_velib(url):
     payload = {}
@@ -17,8 +18,10 @@ def get_velib_Lyon(url):
     return response_json.get("values", [])
 
 
+
+
 vlilles = get_velib("https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=300&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion")
-vLyon = get_velib("https://download.data.grandlyon.com/ws/grandlyon/pvo_patrimoine_voirie.pvostationvelov/all.json?maxfeatures=427&start=1")
+vLyon = get_velib_Lyon("https://download.data.grandlyon.com/ws/grandlyon/pvo_patrimoine_voirie.pvostationvelov/all.json?maxfeatures=427&start=1")
 vParis = get_velib("https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&q=&rows=-1&facet=name&facet=is_installed&facet=is_renting&facet=is_returning&facet=nom_arrondissement_communes")
 vRennes = get_velib("https://data.rennesmetropole.fr/api/records/1.0/search/?dataset=stations_vls&q=&lang=fr&rows=-1")
 
@@ -34,3 +37,13 @@ for vLyon_ in vLyon:
 
 for vRennes_ in vRennes:
     print(json.dumps(vRennes_),end=',\n')
+
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client.vls # or db = client['test-database']
+collection = db.stations # or collection = db['test-collection']
+
+collection.insert_many(vlilles)
+collection.insert_many(vParis)
+collection.insert_many(vLyon)
+collection.insert_many(vRennes)
