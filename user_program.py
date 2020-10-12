1be7c357102152a10e8e55b5a136c3ee8e0b45de
Desprez Thibault <thibault.desprez1@gmail.com>
2020-10-12 11:46:45 +0200

    add part3

---
+++ user_program.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# User program: give available stations name next to the user lat, lon with last data (bikes and stand)
+
+import requests
+import json
+from pprint import pprint
+from pymongo import MongoClient
+
+
+client = MongoClient('mongodb+srv://toto:toto@cluster0.eu1pi.mongodb.net/vls?retryWrites=true&w=majority')
+db = client.vls # or db = client['test-database']
+collection_vlilles = db.vlilles # or collection = db['test-collection']
+collection_stations = db.stations # or collection = db['test-collection']
+
+
+user_lng = input("your lng please :")
+user_lat = input("your lat please :")
+
+closest_station = collection_stations.find({
+    "geo": {
+     "$near": {
+       "$geometry": {
+          "type": "Point" ,
+          "coordinates": [ float(user_lng) , float(user_lat) ]
+       }
+     }
+   }})[0]
+
+latest_data = collection_vlilles.find_one({
+    "name" : closest_station["name"],
+})#.sort([{"record_timestamp" : -1}])
+pprint(latest_data)
