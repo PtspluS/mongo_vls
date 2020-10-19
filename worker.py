8c96fb00bae9f55ceb28a0806f9e9eacfa91e7d1
Desprez Thibault <thibault.desprez1@gmail.com>
2020-10-19 12:36:59 +0200

    bug to write to patch

--- worker.py
+++ worker.py
@@ -13,7 +13,7 @@ client = MongoClient('mongodb+srv://toto:toto@cluster0.eu1pi.mongodb.net/vls?ret
 db = client.vls # or db = client['test-database']
 collection_vlilles = db.vlilles # or collection = db['test-collection']
 
-#collection_vlilles.create_index([('name',1), ('record_timestamp',-1)], unique=True)
+collection_vlilles.create_index([('name',1), ('record_timestamp',-1)], unique=True)
 
 def get_velib(url):
     payload = {}
@@ -36,7 +36,8 @@ for vlib in vlilles:
         "status": vlib["fields"]["etat"] == "EN SERVICE",
         "record_timestamp": datetime.datetime.fromisoformat(vlib["record_timestamp"]) })
 
+
 try:
     print("inserted : " + str(len(collection_vlilles.insert_many(vlilles_format, ordered=False).inserted_ids)))
 except errors.BulkWriteError as e:
-    pass
+    print(e)
